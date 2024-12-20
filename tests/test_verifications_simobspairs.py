f45e23c96342983801d29785beecf5bd353d4b7f
Aljen Uitbeijerse <aljen.uitbeijerse@vortech.nl>
2024-12-20 16:30:48 +0100

    Adapt simobspairs calculation to use intermediate dataset
    Now that the intermediate dataset is available in the datamodel, use that in the simobspairs calculation instead of the input dataset. Because the intermediate dataset already has done part of the calculations needed in simobspairs.

--- tests/test_verifications_simobspairs.py
+++ tests/test_verifications_simobspairs.py
@@ -21,9 +21,10 @@ def test_simobs_output() -> None:
         SimObsVariables(obs=varnames[0], sim=varnames[1]),
         SimObsVariables(obs=varnames[2], sim=varnames[3]),
     ]
-    time = pd.date_range("2014-09-06", periods=4, freq="h")
+    first_time = np.datetime64("2014-09-06") + np.timedelta64(1, "h")
+    time = pd.date_range(first_time, periods=3, freq="h")
     simstarts = pd.date_range("2014-09-06", periods=2, freq="h")
-    x = np.array([1, 2, 3, 4])
+    x = np.array([2, 3, 4])
     y = [x * 2, (x + 4) * 2]
     x1 = [x * 3, x * 4]
     y1 = [
@@ -41,7 +42,7 @@ def test_simobs_output() -> None:
                 x,
             ),
             varnames[1]: (
-                [DataModelCoords.simstart.name, DataModelCoords.time.name],
+                [DataModelCoords.leadtime.name, DataModelCoords.time.name],
                 y,
             ),
             varnames[2]: (
@@ -49,12 +50,13 @@ def test_simobs_output() -> None:
                 x1,
             ),
             varnames[3]: (
-                [extradim, DataModelCoords.simstart.name, DataModelCoords.time.name],
+                [extradim, DataModelCoords.leadtime.name, DataModelCoords.time.name],
                 y1,
             ),
         },
         coords={
             DataModelCoords.simstart.name: simstarts,
+            DataModelCoords.leadtime.name: leadtimes,
             DataModelCoords.time.name: time,
             extradim: [1, 2],
         },
@@ -62,22 +64,17 @@ def test_simobs_output() -> None:
 
     output = _simobs(inputdataset, leadtimes, variablepairs)
 
-    # The time dimension of the output is expected to only have 3 values, the first value from the
-    #  input time should be gone
+    # The time dimension of the output is expected to be equal to the input time
     assert np.array_equal(
         output[DataModelCoords.time.name].data,
-        pd.date_range(
-            np.datetime64("2014-09-06") + np.timedelta64(1, "h"),
-            periods=4 - 1,
-            freq="h",
-        ).values,
+        time.values,
     )
-    # Check the dims of the output variables are in the expected order
+    # Check the dims of the output variables are in the 'expected' order
     #   The order also determines how the values should be compared
     assert output["obsvar1_simobspair_obsvar1"].dims == ("time", "leadtime")
-    assert output["obsvar1_simobspair_simvar1"].dims == ("time", "leadtime")
+    assert output["obsvar1_simobspair_simvar1"].dims == ("leadtime", "time")
     assert output["obsvar2_simobspair_obsvar2"].dims == ("extradim", "time", "leadtime")
-    assert output["obsvar2_simobspair_simvar2"].dims == ("extradim", "time", "leadtime")
+    assert output["obsvar2_simobspair_simvar2"].dims == ("extradim", "leadtime", "time")
     # Check the values of the output variables
     assert np.array_equal(
         output["obsvar1_simobspair_obsvar1"].data,
@@ -86,7 +83,7 @@ def test_simobs_output() -> None:
     )
     assert np.array_equal(
         output["obsvar1_simobspair_simvar1"].data,
-        [[4.0, np.nan], [14.0, 6.0], [np.nan, 16.0]],
+        [[4.0, 6.0, np.nan], [np.nan, 14.0, 16.0]],
         equal_nan=True,
     )
     assert np.array_equal(
@@ -100,8 +97,8 @@ def test_simobs_output() -> None:
     assert np.array_equal(
         output["obsvar2_simobspair_simvar2"].data,
         [
-            [[10.0, np.nan], [42.0, 15.0], [np.nan, 48.0]],
-            [[14.0, np.nan], [56.0, 21.0], [np.nan, 64.0]],
+            [[10.0, 15.0, np.nan], [np.nan, 42.0, 48.0]],
+            [[14.0, 21.0, np.nan], [np.nan, 56.0, 64.0]],
         ],
         equal_nan=True,
     )
