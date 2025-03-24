83ddd58047f3d41c9a62817d84034d200c7cfe1f
jurianbeunk <jurianbeunk@gmail.com>
2025-03-24 15:05:04 +0100

    Update implementation and test for rank histogram

--- src/dpyverification/verifications/rankhistogram.py
+++ src/dpyverification/verifications/rankhistogram.py
@@ -1,5 +1,6 @@
 """Compute the histogram of ranks over the specified dimensions."""
 
+import numpy as np
 import xarray as xr
 from xskillscore import rank_histogram as _rank_histogram  # type: ignore[import-untyped]
 
@@ -23,27 +24,43 @@ def rankhistogram(
     obs = data.intermediate[calcconfig.variablepair.obs]
     sim = data.intermediate[calcconfig.variablepair.sim]
 
-    # https://github.com/Deltares-research/DPyVerification/issues/63
-    if len(sim[DataModelDims.leadtime]) != 1:
-        msg = "Computation of rank histogram for multiple lead times is not yet supported."
-        raise NotImplementedError(msg)
+    rankhistograms_per_leadtime = []
+    for leadtime in sim[DataModelDims.leadtime]:  # type: ignore[misc]
+        # Get a subset of the simulations dataset
+        sim_subset = sim.sel(leadtime=leadtime)  # type: ignore[misc]
+
+        # Compute the rank for
+        _rank: xr.DataArray | xr.Dataset = _rank_histogram(
+            observations=obs,
+            forecasts=sim_subset,
+            dim=calcconfig.reduce_dims,
+            member_dim=DataModelDims.ensemble,
+        )
+
+        # Check a DataArray is returned
+        if not isinstance(_rank, xr.DataArray):  # type: ignore[misc]
+            msg = f"Expected xr.DataArray, got {type(_rank)}"
+            raise TypeError(msg)
+
+        # Set the variable name with specific lead time
+        leadtime_seconds = int(leadtime.values / np.timedelta64(1, "s"))  # type: ignore[misc]
+        name = f"rank_histogram_leadtime_{leadtime_seconds}s"
+        _rank.name = name
+
+        # Set the long_name attribute on the variable
+        # Set units to 1 (CF-compliant indication for dimensionless variable)
+        _rank.attrs = {"long_name": name, "units": 1}
+
+        # Append to the list
+        rankhistograms_per_leadtime.append(_rank)
 
     # Compute
-    _result: xr.DataArray | xr.Dataset = _rank_histogram(
-        observations=obs,
-        forecasts=sim,
-        dim=calcconfig.reduce_dims,
-        member_dim=DataModelDims.ensemble,
-    )
+    data_vars = {k.name: k for k in rankhistograms_per_leadtime}
+    ranks_for_all_leadtimes = xr.Dataset(data_vars=data_vars)
 
     # Set attrs on xr.DataArray
     # For now, store config as dict
     # General config could be stored as xr.Dataset attrs
-    _result.attrs = {str(k): str(v) for k, v in calcconfig.__dict__.items()}  # type: ignore[misc]
+    ranks_for_all_leadtimes.attrs = {str(k): str(v) for k, v in calcconfig.__dict__.items()}  # type: ignore[misc]
 
-    # Covert to xr.Dataset if type xr.DataArray.
-    # since it is required by the add_to_output
-    # method in the pipiline.
-    if hasattr(_result, "to_dataset"):
-        result: xr.Dataset = _result.to_dataset()
-    return result
+    return ranks_for_all_leadtimes
