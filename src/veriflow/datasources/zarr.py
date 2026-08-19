7ef1cd7b8d2e9cf69d0f64eed2936dba99d960d9
jurianbeunk <jurianbeunk@gmail.com>
2026-08-19 16:13:14 +0200

    Add utils.py with function to convert byte string coord to utf8

--- src/veriflow/datasources/zarr.py
+++ src/veriflow/datasources/zarr.py
@@ -9,9 +9,11 @@ from veriflow.constants import (
     DataType,
     SpatialType,
     StandardAttribute,
+    StandardDim,
 )
 from veriflow.datasources.base import BaseDatasource
 from veriflow.types import DataSpec
+from veriflow.utils import convert_byte_string_coord_to_utf8
 
 __all__ = [  # noqa: RUF022
     "Zarr",
@@ -111,12 +113,15 @@ class Zarr(BaseDatasource):
             storage_options=storage_options,
             consolidated=self.config.consolidated,
         )
+
+        dataset = convert_byte_string_coord_to_utf8(dataset, StandardDim.station)  # type:ignore[misc]
+
         # Filter the dataset by configured stations and variables, if any. Set the
         # data_type attribute to the configured value, if any.
         if self.config.stations is not None:
-            dataset = dataset.sel(station=self.config.stations)  # type:ignore[misc]
+            dataset = dataset.sel(station=self.config.stations)
         if self.config.variables is not None:
-            dataset = dataset[self.config.variables]  # type:ignore[misc]
+            dataset = dataset[self.config.variables]
         dataset.attrs["data_type"] = self.config.data_type  # type: ignore[misc]
 
         # Resolve the CRS: a configured 'crs' takes precedence, otherwise the one already
